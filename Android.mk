414e7616d3dd2d08d78080a87da77db7d0ed9a71
Rashed Abdel-Tawab <rashed@linux.com>
2017-02-24 22:43:52 -0500

    X00TD: Add IMS symlinks
    Change-Id: I35a5cd5e2327f82de44fc96934ca5457f28e0afc

--- Android.mk
+++ Android.mk
@@ -24,4 +24,14 @@ $(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware; \
     ln -sf /dev/block/bootdevice/by-name/msadp \
         $(TARGET_OUT_VENDOR)/firmware/msadp)
 
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
 endif
