1877a310b5d98bdbf6bddebac28ec7ebcf14daee
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-27 23:39:54 +0200

    X00TD: Add VNDK HALs
    Change-Id: I1723d5531674b1d84c711a655f6476544bb0a956

--- device.mk
+++ device.mk
@@ -178,6 +178,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# VNDK-SP:
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
