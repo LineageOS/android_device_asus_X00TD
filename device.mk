c3b6ab80447e7c45d0da9050810db6bc92c9e439
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-31 04:40:40 +0200

    X00TD: Build QTI HIDL power 1.1 HAL
    Change-Id: Idb46d7e758bdabd2e30d58e6db6f09ba375c6da4

--- device.mk
+++ device.mk
@@ -136,6 +136,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.1-service-qti
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
