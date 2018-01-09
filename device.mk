9f8930c40e60536c9ecda6fcc6349b248c8fc0bd
Mikhail Naganov <mnaganov@google.com>
2018-01-08 17:05:28 -0800

    X00TD: Uprev soundtrigger to 2.1
    Bug: 68823037
    Change-Id: If634345594ea6d574f49d58e29b48165c3b9458b
    Test: "ok google" and music iq from locked screen
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -81,8 +81,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
