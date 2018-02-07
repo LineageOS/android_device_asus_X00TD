58b43df548b49c307187b28bfe2c3856a313370e
Joey <joey@lineageos.org>
2018-02-07 19:04:19 +0530

    X00TD: build textclassifier
    Change-Id: I50d8173d24c4cab773824107b98929dbbc469783

--- device.mk
+++ device.mk
@@ -182,6 +182,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# TextClassifier smart selection model files
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
