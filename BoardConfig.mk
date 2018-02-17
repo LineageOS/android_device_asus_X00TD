f115efdfddb61aed61dbd42db5c7cf80c3af8a2a
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-17 21:11:27 +0100

    X00TD: wifi: Wait for kernel driver to get ready
    Depends on : https://review.lineageos.org/206958
    
    Change-Id: Ieced27fb0bb5cba6d9850de594b5d4df7977991d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -280,6 +280,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 
 # inherit from the proprietary version
 -include vendor/asus/X00TD/BoardConfigVendor.mk
