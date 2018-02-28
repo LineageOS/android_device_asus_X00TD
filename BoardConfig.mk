0cd3cb53257f8fa231caeae0e42d30c5a2bfd911
Bruno Martins <bgcngm@gmail.com>
2018-02-28 23:19:05 +0000

    X00TD: Use sdFAT for exFAT support
    Change-Id: I6772c485305827cce911f5ce643ff65586b0a356

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,9 @@ ifeq ($(HOST_OS),linux)
 endif
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
+# Extended Filesystem Support
+TARGET_EXFAT_DRIVER := sdfat
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
