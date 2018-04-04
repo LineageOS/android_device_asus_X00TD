be52d5ecd06b51169cb54c2dc9bffb8f75dcfb59
Jorim Jaggi <jjaggi@google.com>
2018-04-04 17:27:54 +0200

    X00TD: enable early phase offsets
    Since we hit GL comp quite frequently during app transitions, we
    need to enable early phase offsets to give it enough time for
    composition.
    
    Test: Open app, make sure vsync-offsets shift
    Bug: 75985430
    Change-Id: I2f85bf72a4b135dce664f7274848f3956a9aaf7a
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -90,6 +90,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
 	debug.sf.enable_hwc_vds=1 \
+	debug.sf.early_phase_offset_ns=5000000 \
 	debug.sf.hw=1 \
 	debug.sf.latch_unsignaled=1 \
 	debug.gralloc.enable_fb_ubwc=1 \
