2414be977e583ab239267dbcfa13fefdfea4077a
Demon Singur <demonsingur@gmail.com>
2018-04-11 14:22:55 +0000

    X00TD: remove AOSP bluetooth impl
    We use the QTI impl which properly loads the bluetooth
    firmware, remove the AOSP one.
    
    Keep using AOSP service because QTI service depends on ANT+
    which doesn't work.
    
    Change-Id: Ie7a31c60c89890567a02bd92adb84eb03eacaf56

--- device.mk
+++ device.mk
@@ -45,7 +45,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service \
     android.hardware.bluetooth@1.0-service-qti \
-    android.hardware.bluetooth@1.0-impl \
     libbt-vendor
 
 # Configs
