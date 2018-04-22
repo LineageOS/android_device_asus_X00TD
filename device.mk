5b78ff503fa05622d6f8708c38a8d61e1221eb82
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-22 12:46:49 +0800

    X00TD: Build Soter

--- device.mk
+++ device.mk
@@ -175,6 +175,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Soter
+PRODUCT_PACKAGES += \
+    soter
+
+PRODUCT_BOOT_JARS += \
+    soter
+
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
