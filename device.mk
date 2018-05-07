371dacf0b4bf25abb914d2001087d2b75056e0cf
ShivamKumarJha <jha.shivam3@gmail.com>
2018-05-07 17:36:08 +0530

    X00TD: HIDL: build android.hidl.manager@1.0-java

--- device.mk
+++ device.mk
@@ -83,7 +83,8 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
 
 # IRQ
 PRODUCT_COPY_FILES += \
