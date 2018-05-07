3d77e87eb29c9ef784ca2b0600332c879fe00870
ShivamKumarJha <jha.shivam3@gmail.com>
2018-05-07 17:36:08 +0530

    X00TD: Build USB HIDL

--- device.mk
+++ device.mk
@@ -441,6 +441,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# USB
+PRODUCT_PACKAGES += \
+	android.hardware.usb@1.0-service
+
 # VNDK-SP:
 PRODUCT_PACKAGES += \
     vndk-sp
