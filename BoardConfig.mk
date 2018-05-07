557245d626fa896a3dbebfc18a37f6feeafe66bd
ShivamKumarJha <jha.shivam3@gmail.com>
2018-05-07 17:36:08 +0530

    X00TD: Optional TWRP support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,6 +235,26 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
+# TWRP Support
+ifeq ($(WITH_TWRP),true)
+TARGET_RECOVERY_DEVICE_DIRS += device/asus/X00TD/twrp
+TARGET_RECOVERY_DEVICE_DIRS += vendor/asus/X00TD/twrp
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_SUPERSU := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_NTFS_3G := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_MAX_BRIGHTNESS := 200
+TW_DEFAULT_BRIGHTNESS := 70
+TW_THEME := portrait_hdpi
+TW_SCREEN_BLANK_ON_BOOT := true
+endif
+
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_X00TD
 TARGET_RECOVERY_DEVICE_MODULES := libinit_X00TD
