5dec45d3cd55e025fb36d22613dc2385bd75b722
ShivamKumarJha <jha.shivam3@gmail.com>
2018-05-07 17:36:08 +0530

    X00TD: Switch to prebuilt sensors
    This fixes broken light & proximity sensor.

--- device.mk
+++ device.mk
@@ -403,12 +403,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
-PRODUCT_PACKAGES += \
-    sendors.qcom \
-    sensors.sdm660 \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
