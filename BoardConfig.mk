33514868587ccf276bfe2865ecba0174db60855d
ShivamKumarJha <jha.shivam3@gmail.com>
2018-05-07 17:36:08 +0530

    X00TD: TARGET_USE_SDCLANG

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,6 +235,9 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
+# Use Snapdragon LLVM, if available
+TARGET_USE_SDCLANG := true
+
 # TWRP Support
 ifeq ($(WITH_TWRP),true)
 TARGET_RECOVERY_DEVICE_DIRS += device/asus/X00TD/twrp
