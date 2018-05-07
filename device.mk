e87d7fa2c8f054593d98df1d91c610c38d48afd4
ShivamKumarJha <jha.shivam3@gmail.com>
2018-05-07 17:36:08 +0530

    X00TD: Build thermal HAL & HIDL

--- device.mk
+++ device.mk
@@ -178,6 +178,12 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.sdm660
+
 # VNDK-SP:
 PRODUCT_PACKAGES += \
     vndk-sp
