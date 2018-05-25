5e67e9cfd63bdf2d6111ddc9cae2e2cafef79e9a
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 00:49:11 +0300

    X00TD: Add powerhint.xml
     * Based on LA.UM.6.2.r1-08700-sdm660.0
     * Required by perf_hint in libqti-perfd-client.so.
    
    Change-Id: Ief38dc242a92de05305452634fe2318e4de42f90

--- device.mk
+++ device.mk
@@ -330,6 +330,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
