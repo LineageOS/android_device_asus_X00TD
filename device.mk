e5fc3a3b52cd127838d872ef8f4b09a8fc3fb171
ShivamKumarJha <jha.shivam3@gmail.com>
2018-06-21 01:08:40 +0530

    X00TD: Import lights HAL

--- device.mk
+++ device.mk
@@ -265,6 +265,14 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.sdm660
+
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
 # Manifest Hack for Indian variant
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest_no_nfc.xml:$(TARGET_COPY_OUT_VENDOR)/manifest_no_nfc.xml \
