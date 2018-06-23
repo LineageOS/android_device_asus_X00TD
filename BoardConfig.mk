8790923659e4dd71d49b6ade45f97f2108ab978f
ShivamKumarJha <jha.shivam3@gmail.com>
2018-06-23 14:48:27 +0530

    Revert "X00TD: wifi: kang configs from sony_nile-common"
    This reverts commit a5b109cf5b28666dbd5b2d9adb4e468b8fe28221.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -291,15 +291,11 @@ BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_BUILT := qca_cld3
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
-WIFI_DRIVER_STATE_CTRL_PARAM := "/sys/kernel/boot_wlan/boot_wlan"
-WIFI_DRIVER_STATE_ON := 1
-WIFI_DRIVER_STATE_OFF := 0
 
 # inherit from the proprietary version
 -include vendor/asus/X00TD/BoardConfigVendor.mk
