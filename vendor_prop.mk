fb5cf9f446c041b70ef3a62e44ca45ebf7f373ea
Bruno Martins <bgcngm@gmail.com>
2018-08-10 23:14:57 +0100

    X00TD: Import Dalvik heap overrides
    Change-Id: Ifac6650a874cf982a6e767da92599d4b6230a637

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -59,6 +59,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.cne.feature=1
 
+# Dalvik overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.heapgrowthlimit=256m \
+	dalvik.vm.heapstartsize=8m \
+	dalvik.vm.heapsize=512m \
+	dalvik.vm.heaptargetutilization=0.75 \
+	dalvik.vm.heapminfree=512k \
+	dalvik.vm.heapmaxfree=8m
+
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
 	drm.service.enabled=true
