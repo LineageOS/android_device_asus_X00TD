9f393f1cfd7c0d07adf0f0e6672c130a088b10ce
LuK1337 <priv.luk@gmail.com>
2018-08-11 16:27:29 +0200

    X00TD: Switch to new NXP NFC
    Change-Id: Idbf8506c5e7c77b56d131bcd857c29a643f46685
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -308,14 +308,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.1-service \
+    android.hardware.nfc@1.1-impl \
     com.android.nfc_extras \
-    com.nxp.nfc.nq \
-    com.nxp.nfc.nq.xml \
-    libnqnfc-nci \
-    libnqp61-jcop-kit \
-    nfc_nci.nqx.default \
-    NQNfcNci \
+    NfcNci \
+    SecureElement \
     Tag \
     vendor.nxp.hardware.nfc@1.0-impl \
     vendor.nxp.hardware.nfc@1.0-service
