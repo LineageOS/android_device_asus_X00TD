522059cd947008ab1a7191e1f8a8e7053963aac0
SagarMakhar <sagarmakhar@gmail.com>
2018-09-03 14:54:01 +0000

    X00TD : use common manifest for all variants
    * remove unsupported releasetools commands
    
    Change-Id: Ifa9f507eb394060b089619dc1295ab913f000983

--- releasetools.py
+++ releasetools.py
@@ -17,27 +17,14 @@ import hashlib
 import common
 import re
 
-def FullOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.input_zip)
-  return
-
-def IncrementalOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.target_zip)
-  return
-
-def AddTrustZoneAssertion(info, input_zip):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-trustzone\s*=\s*(\S+)', android_info)
-  if m:
-    versions = m.group(1).split('|')
-    if len(versions) and '*' not in versions:
-      cmd = 'assert(X00TD.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
-      info.script.AppendExtra(cmd)
-  return
-
 def FullOTA_InstallEnd(info):
-    info.script.Mount("/system");
-    info.script.Mount("/vendor");
-    info.script.AppendExtra('run_program("/sbin/sh", "/tmp/install/bin/device_check.sh");');
-    info.script.Unmount("/system");
-    info.script.Unmount("/vendor");
+  info.script.Mount("/system")
+  info.script.Mount("/vendor")
+  RunCustomScript(info, "device_check.sh", "")
+  info.script.Unmount("/system")
+  info.script.Unmount("/vendor")
+  return
+
+def RunCustomScript(info, name, arg):
+  info.script.AppendExtra(('run_program("/tmp/install/bin/%s", "%s");' % (name, arg)))
+  return
