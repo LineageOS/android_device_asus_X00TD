f843022a2fe3d36f09b4fe5a261d297c73669266
GuaiYiHu <guaiyihu@foxmail.com>
2018-09-28 13:52:17 +0800

    X00TD: wifi: Enable WIFI_HIDL_FEATURE_DUAL_INTERFACE
    See from https://source.android.com/devices/tech/connect/wifi-sta-ap-concurrency
    
    Change-Id: I64f9e7b76e6dffaf59cd78eae4ba3e75f3b13808
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -274,6 +274,7 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # inherit from the proprietary version
 -include vendor/asus/X00TD/BoardConfigVendor.mk
