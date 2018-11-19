b82a3986ef9c5e7c0a8c3d1cf60c76698b2489e6
Albert I <krascgq@outlook.co.id>
2018-11-19 20:14:18 +0800

    X00TD: Update dexpreopt configuration
    Due to LineageOS/android_build@28d0698e8, eng and userdebug builds are
    only having boot and system server dexpreopt-ed thus need to explicitly
    disable WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY on device
    makefile to enable full dexpreopt for userdebug builds.
    
    Change-Id: I8e58de360906e0aa48929c8ef4bef553bef1a286
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,10 +164,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT := true
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # Extended Filesystem Support
 TARGET_EXFAT_DRIVER := sdfat
