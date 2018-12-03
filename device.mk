611cec9a80a7ffe6d8e6623291707ea358ccbeb0
akshatsinha2002 <sinhaakshat2012@gmail.com>
2018-12-03 15:30:50 +0000

    X00TD:Drop aosp keymaster hal
    We use prebuilt qti keymaster hal
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -239,11 +239,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
-# Keymaster HAL
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-
 # Lights
 PRODUCT_PACKAGES += \
     lights.sdm660
