64da1581d52829f1f07786dc0c9022191828d777
Albert I <krascgq@outlook.co.id>
2018-12-16 17:34:45 +0800

    X00TD: gps: Pull update from upstream
    * LA.UM.7.2.r1-05200-sdm660.0
    * Revert android.hardware.gnss back to 1.0
    
    Change-Id: I403e4a5a30ee05aeb7387b1d11a278c131a67860
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -185,7 +185,7 @@ PRODUCT_PACKAGES += \
 
 # GPS / Location
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.1-impl-qti \
+    android.hardware.gnss@1.0-impl-qti \
     libcurl \
     libgnss \
     libgnsspps \
