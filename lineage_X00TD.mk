5c752ddc2e6c7a1675eacfea5861405c648c64f3
Albert I <krascgq@outlook.co.id>
2019-01-19 20:16:01 +0800

    X00TD: Use PRODUCT_SYSTEM_DEFAULT_PROPERTIES to set additional GMS prop
    Change-Id: I66a53cac6adb714d7409b8e03d1b9aac6ac509c4
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -32,6 +32,9 @@ PRODUCT_NAME := lineage_X00TD
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.google.clientidbase.ms=android-asus-tpin
+
 TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00TD
 
--- vendor_prop.mk
+++ vendor_prop.mk
@@ -88,10 +88,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.fuse_sdcard=true
 
-# GMS
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.com.google.clientidbase.ms=android-asus-tpin
-
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
 	debug.sf.enable_hwc_vds=1 \
