c6fe35208603b52c81eab048a30207163bdc6cb8
Albert I <krascgq@outlook.co.id>
2019-02-11 00:14:03 +0800

    X00TD: Update kanged blobs from X01BD 16.2017.1901.046
    CNE/DPM blobs list kanged from LineageOS/android_device_oneplus_oneplus3@a9b6643f
    
    Change-Id: I809db0abdf981c5cfe8dddeb5543f4e563d8fa8c
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- Android.mk
+++ Android.mk
@@ -27,7 +27,7 @@ $(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware; \
         $(TARGET_OUT_VENDOR)/firmware/msadp)
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
