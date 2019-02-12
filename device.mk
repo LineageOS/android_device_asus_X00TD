c2e164af1ea285a532356950f07557255c5d1cc9
Albert I <krascgq@outlook.co.id>
2019-02-12 22:42:46 +0800

    X00TD: Changes to HIDL libs required by Pie RIL
    Change-Id: I826e43b7905126cf91d67c353dcb700d4a138114
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -371,9 +371,9 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0 \
-    android.hardware.radio@1.1 \
-    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
     rild \
     librmnetctl \
     libxml2 \
