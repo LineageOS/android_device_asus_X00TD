701b0112b63db745a77695d53f5373ac15b889f3
Albert I <krascgq@outlook.co.id>
2019-03-03 15:12:32 +0800

    X00TD: Generate thermal-engine.conf for Pie
    :# /vendor/bin/thermal-engine -o > /sdcard/thermal-engine.conf
    
    Pull from device
    
    Change-Id: I76d18e482b54c029e01bfa513ed1fcf1eab4243b
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -435,6 +435,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sdm660
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
