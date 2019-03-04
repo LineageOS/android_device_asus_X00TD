b69f10bbd8443f6434f4edd984550c6f27d995ba
Albert I <krascgq@outlook.co.id>
2019-03-05 02:16:52 +0800

    X00TD: Revert ANT+ HAL back to UART
    As far as I see, many sdm660 devices upgrading from O are dropping
    HIDL version of ANT+, including X01BD which most of our blobs are
    kanged from. Let's do the same here as we might have it dropped as
    well during upgrade to Pie.
    
    Also, update bluetooth and ANT+ stack from X01BD 16.2017.1901.046.
    
    Change-Id: Ifa55dea2d479c920d3be4db67cca2e1ce1278b0c
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -71,7 +71,8 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
+    com.dsi.ant.antradio_library \
+    libantradio
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
