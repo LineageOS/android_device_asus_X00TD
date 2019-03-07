553030f9f2c0f32da978ae5795344c19476c7c94
Albert I <krascgq@outlook.co.id>
2019-03-07 21:56:56 +0800

    X00TD: Update vendor security patch to 2019-02-01
    * Matches stock Pie beta 16.2017.1902.037 / Oreo 15.2016.1902.340.
    
    Change-Id: I5dddb46ed7d66e936dae8a5ab626959db1ad11f6
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,7 +237,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-01-01
+VENDOR_SECURITY_PATCH := 2019-02-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
