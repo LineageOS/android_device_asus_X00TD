Nikesh001/device_asus_X00TD

X00TD: update-sha1sums.py: Update device properties
Change-Id: I8071ea77d0a2b6c698b82519307b94d12a39ba8a
Signed-off-by: Albert I <krascgq@outlook.co.id>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

update-sha1sums.py
@@ -19,8 +19,8 @@
 from hashlib import sha1
 import sys
 
-device='msm8996-common'
-vendor='xiaomi'
+device='X00T'
+vendor='asus'
 
 lines = [ line for line in open('proprietary-files.txt', 'r') ]
 vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'

LineageOS/android_device_asus_X00TD

update-sha1sums.py
@@ -19,8 +19,8 @@
 from hashlib import sha1
 import sys
 
-device='msm8996-common'
-vendor='xiaomi'
+device='X00TD'
+vendor='asus'
 
 lines = [ line for line in open('proprietary-files.txt', 'r') ]
 vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'