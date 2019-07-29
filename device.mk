47c0391db436ab31d23238b02a0a76500d884439
SagarMakhar <sagarmakhar@gmail.com>
2019-07-29 15:42:00 +0000

    X00TD: copy privapp-permission-qti.xml to system instead of vendor
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Ia3ddf7fe4a186d308d5995333f7672a4017675e0

--- device.mk
+++ device.mk
@@ -388,7 +388,7 @@ PRODUCT_BOOT_JARS += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
 # QMI
 PRODUCT_PACKAGES += \
