4eddf2dc538358078b89e1e9f6cdf5e86a0fc4b0
SagarMakhar <sagarmakhar@gmail.com>
2019-08-03 09:53:13 +0000

    X00TD: update blobs from PKQ1.WW_Phone-16.2017.1906.056-20190722 ota
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I7eed126c917790a72d7ea5b5c7b391ab0bbbd67b
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -36,10 +36,10 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00TD
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1016 release-keys"
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1261 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1903.050-20190401:user/release-keys
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1906.056-20190722:user/release-keys
 
 # The following system and vendor props will be set by vendor init
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
