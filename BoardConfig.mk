8c2cfc2fdf98502f5be8587eae15bf2ccee3c434
SagarMakhar <sagarmakhar@gmail.com>
2020-05-12 13:32:09 +0000

    X00TD: Update blobs from 063 ota
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I90afc6c53f805b13f1550138775085d991851356

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -230,7 +230,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-10-05
+VENDOR_SECURITY_PATCH := 2020-04-05
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -36,10 +36,10 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00TD
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1285 release-keys"
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1682 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1910.059-20190920:user/release-keys
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.2004.063-20200407:user/release-keys
 
 # The following system and vendor props will be set by vendor init
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
