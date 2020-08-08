dc765e35dbb76fb478d2feced6c3ec23ad8dae1c
kubersharma001 <kubersharma001@gmail.com>
2020-08-08 09:51:07 +0530

    X00TD: Set height and width for bootanimation
    Change-Id: I8f50fdbeeb7e37ba22bc84a41497c0fe256bf92f

--- device.mk
+++ device.mk
@@ -7,5 +7,9 @@
 # Inherit from sdm660-common
 $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_SCREEN_WIDTH := 1080
+
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/asus/X00TD/X00TD-vendor.mk)
