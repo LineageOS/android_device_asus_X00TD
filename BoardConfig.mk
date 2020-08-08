da935b7951cd47fbb47ab1dc031c645be45c0193
Vivekachooz <vivekachooz@gmail.com>
2020-08-08 09:51:07 +0530

    X00TD: Add firmware requirement
    * Also set device OTA assert
    
    Change-Id: I7ddc6c5a1422bf07bca2b90b091b7ca464e0d27e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,9 @@
 
 DEVICE_PATH := device/asus/X00TD
 
+# Assert
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_OTA_ASSERT_DEVICE := ASUS_X00TD,X00TD,X00T
+
 # Inherit the proprietary files
 -include vendor/asus/X00TD/BoardConfigVendor.mk
