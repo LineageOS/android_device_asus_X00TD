0cb69676bd6669e8a4c4cd7548706836fc9448d5
Vivekachooz <vivekachooz@gmail.com>
2020-08-12 09:51:07 +0530

    X00TD: Create initial proprietary-files.txt
    Change-Id: I7077963beae26add6a70332a38436ed06094b5ff

--- extract-files.sh
+++ extract-files.sh
@@ -7,6 +7,28 @@
 
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
+
+function blob_fixup() {
+    case "${1}" in
+
+    # Fix jar path
+    product/etc/permissions/qti_fingerprint_interface.xml)
+        sed -i 's|/system/framework/|/system/product/framework/|g' "${2}"
+        ;;
+
+    # Rename to fp service avoid conflicts
+    vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service_asus.rc)
+        sed -i 's|android.hardware.biometrics.fingerprint@2.1-service|android.hardware.biometrics.fingerprint@2.1-service_asus|g' "${2}"
+        ;;
+
+    # remove android.hidl.base dependency
+    vendor/lib/hw/camera.sdm660.so)
+        patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
+        ;;
+
+    esac
+}
+
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
     return
 fi
