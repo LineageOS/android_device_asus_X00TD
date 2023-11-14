d693078e34726dc920f7661c65c95162f0d02400
Bruno Martins <bgcngm@gmail.com>
2023-11-14 14:19:47 +0000

    X00TD: Move releasetools requirements to android-info-extra.txt
    This is a non-standard requirement which fastbootd doesn't nativelly
    supports. Move it to the newly introduced file that will only contain
    the requirements used by custom releasetools for the OTA assertions.
    
    Change-Id: Ic3f65a5b9220c914ded468ddc19d2994ba71d964

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,6 @@ include device/asus/sdm660-common/BoardConfigCommon.mk
 DEVICE_PATH := device/asus/X00TD
 
 # Assert
-TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := ASUS_X00TD,X00TD,X00T
 
 # Bootloader
