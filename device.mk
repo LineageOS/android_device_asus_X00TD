bff317881111876b5c5da3ca12f32f36dc1df93a
Vivekachooz <vivekachooz@gmail.com>
2024-01-16 11:02:26 +0530

    X00TD: Build custom fingerprint HIDL
    Change-Id: Ic26083b221d852434073f8be5e8263627f76cec7

--- device.mk
+++ device.mk
@@ -11,6 +11,10 @@ $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.X00TD
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
