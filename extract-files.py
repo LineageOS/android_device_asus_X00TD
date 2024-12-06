c50d1b53f5060637b57103653c8c3ab6586be3c0
ZeeLog <Danjel.Zammit@gmail.com>
2024-12-06 09:54:59 +0100

    X00TD: Add liblog dependency to cdfinger HAL
    - To resolve issue with missing __android_log_print symbol
      when elf checks are enabled.
    
    Change-Id: I58b8bc654e02878f5ef098f429261773ac209b44

--- extract-files.py
+++ extract-files.py
@@ -32,6 +32,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('/system/framework/', '/system/product/framework/'),
     'vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service_asus.rc': blob_fixup()
         .regex_replace('android.hardware.biometrics.fingerprint@2.1-service', 'android.hardware.biometrics.fingerprint@2.1-service_asus'),
+    'vendor/lib64/hw/cdfinger.fingerprint.default.so': blob_fixup()
+        .add_needed('liblog.so'),
     ('vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so', 'vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so'): blob_fixup()
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
 }  # fmt: skip
