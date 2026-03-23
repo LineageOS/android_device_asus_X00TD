5f9a999f41cfcb50ca5b35bacd183e46b1d55670
Debayan Kar <kardebayan3@gmail.com>
2026-03-23 19:33:24 +0000

    X00TD: Migrate to py extract utils
    Change-Id: I8d74f9d4873b994c2d36c75a9f9d82caaaf6427d

---
+++ extract-files.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/asus/sdm660-common',
+    'vendor/asus/sdm660-common',
+    'vendor/qcom/opensource/display',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+blob_fixups: blob_fixups_user_type = {
+    'product/etc/permissions/qti_fingerprint_interface.xml': blob_fixup()
+        .regex_replace('/system/framework/', '/system/product/framework/'),
+    'vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service_asus.rc': blob_fixup()
+        .regex_replace('android.hardware.biometrics.fingerprint@2.1-service', 'android.hardware.biometrics.fingerprint@2.1-service_asus'),
+    ('vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so', 'vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so'): blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'X00TD',
+    'asus',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
